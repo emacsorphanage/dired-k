06a2f024b72ff6a919c340b7348ff6aa4d9bfee8
Syohei YOSHIDA <syohex@gmail.com>
2014-06-18 00:47:11 +0900

    bump up version and update Changes

--- direx-k.el
+++ direx-k.el
@@ -3,8 +3,6 @@
 ;; Copyright (C) 2014 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
-;; URL: https://github.com/syohex/emacs-direx-k
-;; Version: 0.01
 ;; Package-Requires: ((cl-lib "0.5") (emacs "24"))
 
 ;; This program is free software; you can redistribute it and/or modify
