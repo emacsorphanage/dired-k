95302b7218ba4e5e47ea9a5fa995ea72e52461c8
Syohei YOSHIDA <syohex@gmail.com>
2016-11-16 09:51:09 +0900

    Drop older version Emacs 24.1 and 24.2

--- dired-k.el
+++ dired-k.el
@@ -5,7 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-dired-k
 ;; Version: 0.18
-;; Package-Requires: ((cl-lib "0.5") (emacs "24"))
+;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
